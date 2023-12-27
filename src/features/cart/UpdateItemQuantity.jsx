86bce450e27a2f11b904fe7ba97bb7f0dcac15e3
Islam Kamal <islamkamalx1>
2023-12-27 21:13:48 +0200

    uploaded

---
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import { useDispatch } from 'react-redux';
+import Button from '../../ui/Button';
+import { decreaseQuantity, increaseQuantity } from './cartSlice';
+
+const UpdateItemQuantity = ({ pizzaId, currentQuantity }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex items-center gap-2 md:gap-3">
+      <Button type="round" onClick={() => dispatch(decreaseQuantity(pizzaId))}>
+        -
+      </Button>
+      <span className="text-sm font-medium">{currentQuantity}</span>
+      <Button type="round" onClick={() => dispatch(increaseQuantity(pizzaId))}>
+        +
+      </Button>
+    </div>
+  );
+};
+
+export default UpdateItemQuantity;
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,64 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  // Action Creators
+  reducers: {
+    addItem(state, action) {
+      // payload = newItem
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      // payload = pizzaID
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseQuantity(state, action) {
+      // payload = pizzaID
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseQuantity(state, action) {
+      // payload = pizzaID
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseQuantity,
+  decreaseQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+// SELECTORS FUNCTIONS
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0; //if you find the item get the quantity and if no quantity return 0
+
+// reselect [library for enhancing performance of redux]
